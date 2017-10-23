a01c8ed0163cf79d8899a90f5b7b8e19ed236900
Javier Diaz <coderdiaz@gmail.com>
2017-10-23 17:56:02 -0500

    Added exercices for getting started

--- examples/hello-world/app.js
+++ examples/hello-world/app.js
@@ -1,6 +1,36 @@
 new Vue({
   el: '#app',
   data: {
-    message: 'Hello world. Welcome to Vue.js'
-  }
+    message: 'Hello world. Welcome to Vue.js',
+    languages: [
+      {name: 'Javascript'},
+      {name: 'PHP'},
+      {name: 'Go'},
+      {name: 'Java'}
+    ],
+    showElement: true,
+    inputValue: '',
+    user: {
+      firstName: '',
+      lastName: ''
+    },
+    filter: ''
+  },
+  computed: {
+    fullName: function () {
+      return this.user.firstName + ' ' + this.user.lastName;
+    },
+    filterLanguages: function () {
+      var filter = this.filter;
+      return (this.filter == '') ? this.languages
+      : this.languages.filter(function (language) {
+        return language.name.toLowerCase() === filter.toLowerCase();
+      });
+    }
+  },
+  methods: {
+    toggleElement: function () {
+      this.showElement = !this.showElement;
+    }
+  } 
 });
