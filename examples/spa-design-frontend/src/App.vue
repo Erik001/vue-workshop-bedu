76982d5a998d5818201bbd83c4fecbfb4d39bfec
Javier Diaz <coderdiaz@gmail.com>
2017-10-23 19:06:36 -0500

    Minor fixes

--- examples/spa-design-frontend/src/App.vue
+++ examples/spa-design-frontend/src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
+    <header-component></header-component>
     <img src="./assets/logo.png">
     <HelloWorld/>
   </div>
 </template>
 
 <script>
+import HeaderComponent from '@/components/header/header'
 import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    HeaderComponent
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss" src="./main.scss"></style>
