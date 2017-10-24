af5d49b1f05ede06711a0fcc9e0f8c29f52092fc
Javier Diaz <coderdiaz@gmail.com>
2017-10-24 18:40:37 -0500

    Added mounted function to get data

--- examples/spa-stars-wars/src/views/home/home.vue
+++ examples/spa-stars-wars/src/views/home/home.vue
@@ -1,9 +1,17 @@
 <template src="./home.html"></template>
+
 <script>
+import {getPeople} from '@/services/people'
 export default {
   name: 'Home',
+  mounted () {
+    getPeople().then(response => {
+      this.people = response.data.results
+    }).catch(err => console.error(err))
+  },
   data () {
     return {
+      people: [],
       message: 'Welcome to home view'
     }
   }
