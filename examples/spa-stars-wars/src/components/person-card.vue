05ec898bf5f8e9550eed1f67e36b5abd8c4fac34
Javier Diaz <coderdiaz@gmail.com>
2017-10-24 23:08:15 -0500

    Added person card component

---
+++ examples/spa-stars-wars/src/components/person-card.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col-4">
+    <div class="card sw-card text-center">
+      <div class="card-body">
+        <div class="sw-avatar align-middle">
+          <img v-if="person.gender === 'female'" src="../assets/female.png" width="128">
+          <img v-else src="../assets/male.png" width="128">
+        </div>
+        <h4 class="card-title">{{person.name}}</h4>
+        <h6 class="card-subtitle text-muted">{{person.birth_year}}</h6>
+        <p class="card-text"></p>
+        <hr>
+        <div class="text-center">
+          <button class="btn btn-dark">View profile</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PersonCard',
+  props: {
+    person: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
