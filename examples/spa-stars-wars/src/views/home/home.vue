97405b22d379596d4a135bb6c48d9a6d54d44889
Javier Diaz <coderdiaz@gmail.com>
2017-10-24 21:55:45 -0500

    More functionality added

--- examples/spa-stars-wars/src/views/home/home.vue
+++ examples/spa-stars-wars/src/views/home/home.vue
@@ -5,14 +5,36 @@ import {getPeople} from '@/services/people'
 export default {
   name: 'Home',
   mounted () {
-    getPeople().then(response => {
-      this.people = response.data.results
-    }).catch(err => console.error(err))
+    this.isLoading = true
+    this.getPeople()
   },
   data () {
     return {
       people: [],
-      message: 'Welcome to home view'
+      message: 'Welcome to home view',
+      isLoading: false,
+      search: {
+        person: ''
+      },
+      currentPage: 1
+    }
+  },
+  computed: {
+    contentClassObject () {
+      return {
+        'sw-loading': this.isLoading
+      }
+    }
+  },
+  methods: {
+    getPeople (page = 1) {
+      getPeople(page).then(response => {
+        this.people = response.data.results
+        this.isLoading = false
+      }).catch(err => {
+        console.error(err)
+        this.isLoading = false
+      })
     }
   }
 }
