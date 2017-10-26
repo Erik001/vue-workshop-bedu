1ce771e34a56e8534a9e6339e552f14a340b613f
Javier Diaz <coderdiaz@gmail.com>
2017-10-25 23:39:19 -0500

    Added function to get a person by id

--- examples/spa-stars-wars/src/services/people.js
+++ examples/spa-stars-wars/src/services/people.js
@@ -7,3 +7,12 @@ import axios from 'axios'
 export function getPeople (page) {
   return axios.get(`https://swapi.co/api/people/?page=${page}`)
 }
+
+/**
+ * Function to get a person by id
+ * @param {Number} personId
+ * @return AxiosPromise
+ */
+export function getPerson (personId) {
+  return axios.get(`https://swapi.co/api/people/${personId}`)
+}
