66c907b54f303805084d457242cf9a2ad01d9e1c
Javier Diaz <coderdiaz@gmail.com>
2017-10-25 23:39:36 -0500

    Added route for show profile

--- examples/spa-stars-wars/src/router/index.js
+++ examples/spa-stars-wars/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HomeView from '@/views/home/home'
+import PersonView from '@/views/person/person'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: HomeView
+    },
+    {
+      path: '/person/:id',
+      name: 'Person',
+      component: PersonView
     }
   ]
 })
